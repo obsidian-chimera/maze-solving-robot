8c91b28ea4618be0653c5ea4e6eee8240da2f8bd
Ruier Li <rl1685007@gmail.com>
2026-05-28 13:24:42 +0100

    Set up robot framework and main loop functionality
    Initialize sensors, motors, and FSM in setup. Read sensor data and execute actions in loop.

--- arduino/main_robot/main_robot.ino
+++ arduino/main_robot/main_robot.ino
@@ -1,7 +1,32 @@
+#include "config.h"
+#include "sensors.h"
+#include "motors.h"
+#include "fsm.h"
+
+unsigned long lastDebugTime = 0;
+
 void setup() {
-  // put your setup code here
+  Serial.begin(9600);
+
+  initSensors();
+  initMotors();
+  initFSM();
+
+  Serial.println("Robot framework started.");
 }
 
 void loop() {
-  // put your main code here
+  SensorData s = readSensors();
+
+  updateDecision(s);
+  executeAction();
+
+  if (millis() - lastDebugTime > DEBUG_INTERVAL_MS) {
+    printSensors(s);
+
+    Serial.print("ACTION = ");
+    Serial.println(getStateName(getCurrentState()));
+
+    lastDebugTime = millis();
+  }
 }
